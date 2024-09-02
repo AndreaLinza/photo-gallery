1bac8adc8196efe0d73d8d9d8cbc67b20d2b6e5c
AndreaLinza <andrealinza@proton.me>
2024-09-02 12:01:03 +0200

    change local language faker and rollback data in table User with Factory

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,27 +16,30 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        for($i=0; $i<30; $i++) {
-            $name = fake()->name();
-            $email = fake()->unique()->safeEmail();
-            $password = fake()->unique()->password();
-            /* Due modi per inserire dati nella tabella
-                Tramite segnaposti dove andiamo a specificare in che tabella inserire i dati ed il relativo array dati
-                    oppure
-                Tramite il metodo table('nometabella')->insert() dove si passa l'array con i dati e le variabili
-            */
-            /* 1° METODO
-            $sql = 'insert into users (name, email, password, created_at, email_verified_at) values(?, ?, ?, ?, ?)';
-            DB::insert($sql, [$name, $email, Hash::make($password), Carbon::now(), Carbon::now() ]);
-            */
-            //2° METODO
-            DB::table('users')->insert([
-                'name' => $name,
-                'email' => $email,
-                'password' => Hash::make($password),
-                'created_at' => Carbon::now(),
-                'email_verified_at' => Carbon::now(),
-            ]);
-        }
+        // for($i=0; $i<30; $i++) {
+        //     $name = fake()->name();
+        //     $email = fake()->unique()->safeEmail();
+        //     $password = fake()->unique()->password();
+        //     /* Due modi per inserire dati nella tabella
+        //         Tramite segnaposti dove andiamo a specificare in che tabella inserire i dati ed il relativo array dati
+        //             oppure
+        //         Tramite il metodo table('nometabella')->insert() dove si passa l'array con i dati e le variabili
+        //     */
+        //     /* 1° METODO
+        //     $sql = 'insert into users (name, email, password, created_at, email_verified_at) values(?, ?, ?, ?, ?)';
+        //     DB::insert($sql, [$name, $email, Hash::make($password), Carbon::now(), Carbon::now() ]);
+        //     */
+        //     //2° METODO
+        //     DB::table('users')->insert([
+        //         'name' => $name,
+        //         'email' => $email,
+        //         'password' => Hash::make($password),
+        //         'created_at' => Carbon::now(),
+        //         'email_verified_at' => Carbon::now(),
+        //     ]);
+        // }
+
+        // ALTRIMENTI SI UTILIZZA LA FACTORY
+        \App\Models\User::factory(30)->create();
     }
 }
